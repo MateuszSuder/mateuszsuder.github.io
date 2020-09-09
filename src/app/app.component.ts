dca065c8701607f59a69ccf3e20ce7beeac8aff9
Mateusz Suder <matisu999@gmail.com>
2020-09-10 01:57:19 +0200

    Fade in on enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { trigger, style, transition, animate, query } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        query('.bookmark', style({ opacity: 0 })),
+        query('h1', style({ opacity: 0 })),
+        query('h2', style({ opacity: 0 })),
+        query('#info-text, #skill-text', style({ opacity: 0 })),
+        query('#header-container', style({opacity: 0})),
+
+        query('.bookmark', animate(500, style({ opacity: 1 }))),
+        query('h1', animate(700, style({ opacity: 1 }))),
+        query('h2', animate(700, style({ opacity: 1 }))),
+        query('#info-text, #skill-text', animate(1000, style({ opacity: 1 }))),
+        query('#header-container', animate(1500, style({ opacity: 1 }))),
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'portfolio';
 }
