5167e57552b0d7213270c327103a18e28e818d01
Mateusz Suder <matisu999@gmail.com>
2020-09-11 05:55:28 +0200

    Added ids of sections

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   bookmarks: string[] = ['About me', 'Skills', 'Projects', 'Contact'];
+  idBookmarks: string[] = ['info', 'skill-info', 'projects-info', 'contact-info'];
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  scroll(item: string): void{
+    document.getElementById(this.idBookmarks[this.bookmarks.indexOf(item)]).scrollIntoView({behavior: 'smooth', block: 'start'});
+  }
 }
