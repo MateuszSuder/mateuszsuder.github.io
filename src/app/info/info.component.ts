8879e91d4899590982ad4e306cb9f870daa91a67
Mateusz Suder <matisu999@gmail.com>
2020-09-11 05:56:18 +0200

    Visual change

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -6,5 +6,4 @@ import { Component} from '@angular/core';
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss'],
 })
-export class InfoComponent{
-}
+export class InfoComponent{}
