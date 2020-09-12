76bcde6e2de2afc1e5ebb29d0ff8950a951baef8
Mateusz Suder <matisu999@gmail.com>
2020-09-12 19:45:56 +0200

    Added elements to animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,16 @@ import { trigger, style, transition, animate, query } from '@angular/animations'
     trigger('fade', [
       transition(':enter', [
         query('.bookmark', style({ opacity: 0 })),
-        query('h1', style({ opacity: 0 })),
-        query('h2', style({ opacity: 0 })),
-        query('#info-text, #skill-text', style({ opacity: 0 })),
-        query('#header-container, #skills-frame', style({opacity: 0})),
+        query('#inner-info > h1, .name', style({ opacity: 0 })),
+        query('#inner-info > h2', style({ opacity: 0 })),
+        query('#info-text, #skill-text, .description, #contact-text', style({ opacity: 0 })),
+        query('#header-container, #skills-frame, .preview, .project-bottom, #contact-list', style({opacity: 0})),
 
         query('.bookmark', animate(500, style({ opacity: 1 }))),
-        query('h1', animate(700, style({ opacity: 1 }))),
-        query('h2', animate(700, style({ opacity: 1 }))),
-        query('#info-text, #skill-text', animate(1000, style({ opacity: 1 }))),
-        query('#header-container, #skills-frame', animate(1500, style({ opacity: 1 }))),
+        query('h1, .name', animate(700, style({ opacity: 1 }))),
+        query('h2', animate(500, style({ opacity: 1 }))),
+        query('#info-text, #skill-text, .description, #contact-text, #contact-list', animate(1000, style({ opacity: 1 }))),
+        query('#header-container, #skills-frame, .preview, .project-bottom', animate(1500, style({ opacity: 1 }))),
       ])
     ])
   ]
